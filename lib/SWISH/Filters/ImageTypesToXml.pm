c98d57222661b43a82b3171948199f9c72204f3b
Logan <loganbell@gmail.com>
2011-12-27 09:10:42 -0800

    Updated version #

--- lib/SWISH/Filters/ImageTypesToXml.pm
+++ lib/SWISH/Filters/ImageTypesToXml.pm
@@ -10,12 +10,11 @@ SWISH::Filters::ImageTypesToXml - A filter that applies Imager::ImageTypes to in
 
 =head1 VERSION
 
-Version 0.03
+Version 0.04
 
 =cut
 
-our $VERSION = '0.03';
-
+our $VERSION = '0.04';
 
 =head1 SYNOPSIS
 
